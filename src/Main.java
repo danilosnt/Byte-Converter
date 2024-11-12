6c790a31ade59c96ba0da2bea943c15781184d1d
danilosnt <daniloddocarmo16@icloud.com>
2024-11-12 13:51:23 -0300

    add PB, EB, ZB ,YB

--- src/Main.java
+++ src/Main.java
@@ -29,11 +29,35 @@ public class Main {
 
     public static String formatBytes(BigInteger bytes) {
         BigInteger oneKB = new BigInteger("1024");
-        BigInteger oneMB = oneKB.multiply(oneKB); // 1024 KB
-        BigInteger oneGB = oneMB.multiply(oneKB); // 1024 MB
-        BigInteger oneTB = oneGB.multiply(oneKB); // 1024 GB
+        BigInteger oneMB = oneKB.multiply(oneKB);
+        BigInteger oneGB = oneMB.multiply(oneKB);
+        BigInteger oneTB = oneGB.multiply(oneKB);
+        BigInteger onePB = oneTB.multiply(oneKB);
+        BigInteger oneEB = onePB.multiply(oneKB);
+        BigInteger oneZB = oneEB.multiply(oneKB);
+        BigInteger oneYB = oneZB.multiply(oneKB);
 
-        if (bytes.compareTo(oneTB) >= 0) {
+        if (bytes.compareTo(oneYB) >= 0 ) {
+            BigDecimal yb = new BigDecimal(bytes).divide(new BigDecimal(oneYB), 2, RoundingMode.HALF_UP);
+            return yb.toString().replace(".", ",") + " YB";
+        }
+
+        else if (bytes.compareTo(oneZB) >= 0 ) {
+            BigDecimal zb = new BigDecimal(bytes).divide(new BigDecimal(oneZB),2, RoundingMode.HALF_UP);
+            return zb.toString().replace(".", ",") + " ZB";
+        }
+
+        else if (bytes.compareTo(oneEB) >= 0 ) {
+            BigDecimal eb = new BigDecimal(bytes).divide(new BigDecimal(oneEB), 2, RoundingMode.HALF_UP);
+            return eb.toString().replace(".", ",") + " EB";
+        }
+
+        else if (bytes.compareTo(onePB) >= 0 ) {
+            BigDecimal pb = new BigDecimal(bytes).divide(new BigDecimal(onePB), 2, RoundingMode.HALF_UP);
+            return pb.toString().replace(".", ",") + " PB";
+        }
+
+        else if (bytes.compareTo(oneTB) >= 0) {
             BigDecimal tb = new BigDecimal(bytes).divide(new BigDecimal(oneTB), 2, RoundingMode.HALF_UP);
             return tb.toString().replace(".", ",") + " TB";
         }
